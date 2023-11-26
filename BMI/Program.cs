23178c2181c58f9fcbc119d983cf71b65b9c6f54
ST12-217\Student <ryszard.kuczowic@chorzow.merito.pl>
2023-11-26 13:20:20 +0100

    Dodaj pliki projektów.

---
+++ BMI/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+
+    public static void Bmi()
+    {
+        Console.WriteLine("Podaj swoją wagę w kilogramach: ");
+        double waga = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Podaj swój wzrost w metrach np. 1,70: ");
+        double wzrost = Convert.ToDouble(Console.ReadLine());
+        double bmi = waga / (wzrost * wzrost);
+        Console.WriteLine(bmi);
+
+    }
+    static void Main(string[] args)
+    {
+        Bmi();
+    }
+}
